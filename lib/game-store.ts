4ed232f88bdc16b4fd743adc631534603ba5f08b
sebastian eng <sebastian.e.eng@icloud.com>
2026-02-10 12:23:59 +0100

    roky

--- lib/game-store.ts
+++ lib/game-store.ts
@@ -627,6 +627,7 @@ export function useGameActions() {
               : v,
           )),
         }
+        pendingRoutes.delete(veh.id)
         emit()
         
         pendingRoutes.delete(veh.id)
